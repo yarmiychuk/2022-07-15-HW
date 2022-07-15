51e60346072461ebbe86abc55508e2761441fd8b
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-15 17:45:00 +0300

    Distance 3D app added

---
+++ 21-Distance/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве.
+*/
+
+double GetDistance(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    int xDistSquared = (xA - xB) * (xA - xB);
+    int yDistSquared = (yA - yB) * (yA - yB);
+    int zDistSquared = (zA - zB) * (zA - zB);
+
+    return Math.Sqrt(xDistSquared + yDistSquared + yDistSquared); 
+}
+
+int GetCoordinate(string point, string axis)
+{
+    Console.Write($"Введите координату для точки {point} по оси {axis}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Программа для вычиления расстояния между двумя точками в 3D");
+
+int xA = GetCoordinate("A", "X");
+int yA = GetCoordinate("A", "Y");
+int zA = GetCoordinate("A", "Z");
+
+int xB = GetCoordinate("B", "X");
+int yB = GetCoordinate("B", "Y");
+int zB = GetCoordinate("B", "Z");
+
+Console.WriteLine($"Расстояние между точками равно {GetDistance(xA, yA, zA, xB, yB, zB)}");
