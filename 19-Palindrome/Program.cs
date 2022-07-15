c849fdf169bc79a7d6e54b0d80a95f62fa8df4c2
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-15 17:05:58 +0300

    Palindrome app added

---
+++ 19-Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом
+*/
+
+bool IsPolindrome(int number)
+{
+    return number / 1000 == number / 10 % 10 + number % 10 * 10;
+}
+
+bool IsCorrectInput(int number)
+{
+    return(number >= 10000 && number <= 99999);
+}
+
+string GetTextResult(int number)
+{
+    string text = $"Число {number} ";
+    if (!IsPolindrome(number)) text+= "НЕ ";
+    return text += "является палиндромом.";
+}
+
+int number;
+
+Console.WriteLine("Является ли число палиндромом?");
+Console.Write("Введите пятизначное число: ");
+
+while (!IsCorrectInput(number = Convert.ToInt32(Console.ReadLine())))
+{
+    Console.Write("Вы ввели неправильное число! Повторите ввод: ");
+}
+
+Console.WriteLine(GetTextResult(number));
