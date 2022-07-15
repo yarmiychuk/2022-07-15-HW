335fc0f50fcad88776644e364776f1a8f21c5970
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-15 19:36:08 +0300

    Pow app added

---
+++ 23-Pow/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+*/
+
+void ShowPowFromNumbers(int number)
+{
+    int current = 1;
+    while (current <= number)
+    {
+        Console.Write(Math.Pow(current, 3));
+        if (current < number) Console.Write(", ");
+        else Console.WriteLine();
+        current++;
+    }
+}
+
+bool IsCorrectInput(int number)
+{
+    return number > 0;
+}
+
+int number;
+
+Console.WriteLine("Отображение кубов чисел от 1 до N");
+Console.Write("Введите число: ");
+
+while (!IsCorrectInput(number = Convert.ToInt32(Console.ReadLine())))
+{
+    Console.Write("Вы ввели неправильное число! Повторите ввод: ");
+}
+
+ShowPowFromNumbers(number);
